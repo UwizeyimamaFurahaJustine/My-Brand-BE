eea66eb8b46bb7f8b2ebc05f51cc66f47da4d3bf
UwizeyimanaFurahaJustine <uwizefuraha@gmail.com>
2024-03-21 11:13:54 +0200

    date

--- src/models/Blog.ts
+++ src/models/Blog.ts
@@ -1,22 +1,21 @@
 import mongoose, { Schema, Model, Document } from 'mongoose';
 
-
 interface IBlog extends Document {
     title: string;
     description: string;
     image: string;
     likesNo: number;
     commentsNo: number;
-   
+    createdAt: Date; // Add createdAt field
 }
 
 const BlogSchema = new Schema({
     title: { type: String, required: true },
     description: { type: String, required: true },
-    image: {type: String, required: true},
+    image: { type: String, required: true },
     likesNo: { type: Number, default: 0, required: true },    
-    commentsNo: { type: Number, default: 0, required: true } ,
-   
+    commentsNo: { type: Number, default: 0, required: true },
+    createdAt: { type: Date, default: Date.now, immutable: true } // Add immutable: true to prevent updates
 });
 
 const Blog: Model<IBlog> = mongoose.model<IBlog>('Blog', BlogSchema);
